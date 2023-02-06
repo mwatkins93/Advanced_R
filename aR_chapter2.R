e87291a1cc285ebf6e1bb0475976136b32138819
Matt <96960455+ForevaR@users.noreply.github.com>
2023-02-06 03:30:46 -0500

    upd - adjusted file names; updated quiz results.

--- aR_chapter2.R
+++ aR_chapter2.R
@@ -17,7 +17,7 @@ options(stringsASfactors = FALSE, scipen = 999, encoding = "UTF-8")
 library(tidyverse)
 library(readxl)
 
-## 2. Chapter 1 Quiz
+## 2. Chapter 1 Quiz ----
 
 ### 2.01 - Given the following data frame, how do I create a new column called “3” that contains the sum of 1 and 2? You may only use $, not [[. What makes 1, 2, and 3 challenging as variable names? ----
 
@@ -31,12 +31,13 @@ df$"3" <- df$"1" + df$"2" # one correct answer
 
 x <- runif(1e6)
 y <- list(x, x, x) # answer is 24MB)
+object.size(y)
 
 ### 2.03 - On which line does "a" get copied in the following example?
 
 a <- c(1, 5, 3, 2) # create a vector and store it in a
-b <- a # now store a in b (a copied here)
-b[[1]] <- 10 # change the first element in b to 10
+b <- a # now store a in b
+b[[1]] <- 10 # a copied here when b is modified
 
-### 3/3 on quiz; can move to Chapter 2.
+### 2/3 on quiz.
 
---
+++ aR_chapter3.R
@@ -0,0 +1,21 @@
+#########################
+## Name: Matt Watkins
+## Date: Feb. 6th, 2023
+## Project: Advanced R
+## Objective: Chapter 3
+## Inputs:
+## Outputs:
+#########################
+
+## 0. NOTES ----
+
+## 1. PREPARE ----
+
+rm(list=ls())
+options(stringsASfactors = FALSE, scipen = 999, encoding = "UTF-8")
+
+library(tidyverse)
+library(readxl)
+
+## 2. Chapter 3 Quiz ----
+
